3332fcb693c7693282066f79076a0d7f5047b9ef
AbdullahDurrani-0011 <abdullah.techtribe@gmail.com>
2025-07-08 22:15:29 +0500

    Complete UI

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-<script>
-    
-</script>
+    const inputElement = document.getElementById('note-title');
+    const inputValue = inputElement.value;
+    const inputElement1 = document.getElementById('note-body');
+    const inputValue1 = inputElement.value;
