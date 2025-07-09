4e04d2543c154026b6966fa994520f6747ab8848
AbdullahDurrani-0011 <abdullah.techtribe@gmail.com>
2025-07-09 21:53:08 +0500

    Scripts

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-    const inputElement = document.getElementById('note-title');
-    const inputValue = inputElement.value;
-    const inputElement1 = document.getElementById('note-body');
-    const inputValue1 = inputElement.value;
+const notes = [];
+let myFuntion = document
+  .getElementById("myFunction")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let noteTitle = document.getElementById("note-title").value;
+
+    let noteBody = document.getElementById("note-body").value;
+
+    notes.push({ title: noteTitle, body: noteBody });
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
+
+console.log(document.getElementById("myFunction"));
+
+// let getNotes = () => {
+//   // get notes from local storage
+//   const storedNotes = localStorage.getItem("notes");
+//   console.log(storedNotes);
+//   const outputContainer = document.getElementById("output");
+
+//   outputContainer.innerHTML = storedNotes;
+// };
+
+document.getElementsById("btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("clicked");
+});
