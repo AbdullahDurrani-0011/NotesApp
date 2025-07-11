f906bedb1b88ae3cbd9e6619479cecc6dd72ddff
AbdullahDurrani-0011 <abdullah.techtribe@gmail.com>
2025-07-11 22:01:40 +0500

    FridayUpdate

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const form = document.getElementById("myFunction");
-const titleInput = document.getElementById("note-title");
-const bodyInput = document.getElementById("note-body");
+let titleInput = document.getElementById("note-title");
+let bodyInput = document.getElementById("note-body");
 const addNote = document.getElementById("addBtn");
 
-const node = document.getElementById("node-container");
+// let emptyDiv = document.getElementById("node-container");
 
+let notes = [];
+
+// const addBtn = document.getElementById("addBtn");
 addNote.addEventListener("click", function (e) {
   e.preventDefault();
-  const title = document.getElementById("note-title").value;
-  const body = document.getElementById("note-body").value;
+  // const title = document.getElementById("note-title").value;
+  // const body = document.getElementById("note-body").value;
+
+  if (titleInput.value === "" || bodyInput.value === "") {
+    return 0;
+  }
+
   const id = Math.round(Math.random() * 1000);
-  const notes = {
+
+  const note = {
     id: id,
     title: title,
     body: body,
   };
-  console.log(notes);
-  // node.innerHTML= notes.title;
+
+  notes.push(note);
+
+  for (let i = 0; i < notes.length; i++) {
+    console.log(notes[i]);
+  }
+  titleInput.value = "";
+  bodyInput.value = "";
+
+  // document.getElementById("myFunction").innerHTML =
+  //   note.id + ", " + note.title + ", " + note.body;
+
+  // console.log(notes);
 });
 
+// node.innerHTML= notes.title;
